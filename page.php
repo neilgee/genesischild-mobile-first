5d64f432ecce1bc6cfe59bf10ee666d7f4a74b51
Neil Gee <neil@wpbeaches.com>
2016-03-15 17:48:18 +1100

    code clean up, add theme defaults, upgrade normalise

--- page.php
+++ page.php
@@ -1,11 +1,12 @@
 <?php
+/**
+ * genesischild-mobile-first page template.
+ *
+ * @package genesischild-mobile-first
+ * @author  NeilGee
+ * @license GPL-2.0+
+ * @link    http://wpbeaches.com/
+ */
 
-/************************************************
-* @package genesischild-mobile-first
-* @author  NeilGee
-* @license GPL-2.0+
-* @link    http://wpbeaches.com/
-************************************************/
-
-//* This file handles pages, but only exists for the sake of child theme forward compatibility.
+// This file handles pages, but only exists for the sake of child theme forward compatibility.
 genesis();
